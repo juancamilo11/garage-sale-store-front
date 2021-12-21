8fc8b1ffdaee1feb27bb93605bb38dab0141888d
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-12-21 18:28:39 -0500

    Se creó el store de la aplicación

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,4 @@
+export default authReducer = (state = initialState, action) => {
+  switch (action.type) {
+  }
+};
---
+++ src/store/store.js
@@ -0,0 +1,23 @@
+import {
+  aplyMiddleware,
+  combineReducers,
+  createStore,
+  compose,
+  applyMiddleware,
+} from "redux";
+import thunk from "redux-thunk";
+import authReducer from "../reducers/authReducer";
+
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const reducers = combineReducers({
+  auth: authReducer,
+});
+
+export default store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunk))
+);
