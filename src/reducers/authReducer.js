26f372fd37b507f6f1353b9860ce4cdc8c705924
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-12-21 19:06:57 -0500

    Se creó el reducer para la autenticación de usuario

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,4 +1,23 @@
+import types from "../types/types";
+
+/*
+if(logged) -> {uid: 3DN43784387MCM7, name: Juan Camilo Cardona Calderón, photoUrl: https://lh3.googleusercontent.com/a-/AOh14GjnkTKE1MwBx1jBXLj6SCsCSUANvgmn28L0yh31wg=s96-c-rg-br100}
+
+if(!logged) -> {} 
+
+*/
 export default authReducer = (state = initialState, action) => {
   switch (action.type) {
+    case types.authLogin:
+      return {
+        ...state,
+        uid: action.payload.uid,
+        name: action.payload.name,
+        photoUrl: action.payload.photoUrl,
+      };
+    case types.authLogout:
+      return {};
+    default:
+      return state;
   }
 };
---
+++ src/types/types.js
@@ -0,0 +1,4 @@
+export default types = {
+  authLogin: "[auth] login",
+  authLogout: "[auth] logout",
+};
