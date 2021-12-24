8967a24cdf78ac7ab96cda5fd9623a85e63fd5e5
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-12-24 12:49:32 -0500

    Mejora del componente HomeUserProfile

--- src/pages/userProfile/UserProfilePage.jsx
+++ src/pages/userProfile/UserProfilePage.jsx
@@ -5,6 +5,19 @@ const UserProfilePage = () => {
   return (
     <div className="userprofile__main-container">
       <HomeUserProfile />
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
+      <h1>hola</h1>
     </div>
   );
 };
