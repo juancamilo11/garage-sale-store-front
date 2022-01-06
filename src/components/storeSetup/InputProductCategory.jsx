020911459f0c79dd484553151fa6616c43129735
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-01-06 00:32:59 -0500

    Separación del componente para agregar categorías form 3

---
+++ src/components/storeSetup/InputProductCategory.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import section03Validator from "../../helpers/SetupStoreSection03Validator";
+import useForm from "../../hooks/useForm";
+
+const InputProductCategory = ({ handleInputChange, setErrorsState }) => {
+  const handleInputValidation = (e) => {
+    handleCategoryInputChange(e);
+    section03Validator(e, setErrorsState);
+  };
+
+  const [formValues, handleCategoryInputChange, resetForm] = useForm({
+    categoryName: "",
+    categoryImage: "",
+  });
+
+  const { categoryName, categoryImage } = formValues;
+
+  const handleImageToLoad = (e) => {
+    e.preventDefault();
+    document.getElementById("store-setup__input-category-img").click();
+  };
+
+  const handleSubmitNewCategory = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h2 className="store-setup__section-enum mt-5">
+        3. Creación de los productos para la venta
+      </h2>
+      <form onsubmit={handleSubmitNewCategory}>
+        <div className="store-setup__product-category-container">
+          <label htmlFor="categoryName" className="store-setup__input-label">
+            Nombre de la categoria
+          </label>
+          <input
+            type="text"
+            name="categoryName"
+            id="categoryName"
+            className="store-setup__input"
+            autoComplete="off"
+            value={categoryName}
+            onChange={handleInputValidation}
+          />
+          <div className="store-input-image-container mt-3">
+            <label
+              htmlFor="productCategory"
+              className="store-setup__input-label"
+            >
+              Imágen representativa
+            </label>
+            <button
+              className="store-setup__category-img-button"
+              id="category-button"
+              onClick={handleImageToLoad}
+            >
+              Carga un archivo
+            </button>
+            <input
+              type="file"
+              name="categoryImage"
+              className="store-setup__input-images"
+              id="store-setup__input-category-img"
+              value={categoryImage}
+              onChange={handleInputValidation}
+            />
+          </div>
+          <div className="store-setup__centered-container">
+            <button className="store-setup__button-update" type="submit">
+              Agregar nueva categoría
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default InputProductCategory;
--- src/pages/storeSetup/FormSection03.jsx
+++ src/pages/storeSetup/FormSection03.jsx
@@ -17,6 +17,7 @@ import storeSetupReducer from "./../../reducers/storeSetupReducer";
 import useForm from "../../hooks/useForm";
 import ProductTagList from "../../components/storeSetup/ProductTagList";
 import Swal from "sweetalert2";
+import InputProductCategory from "../../components/storeSetup/InputProductCategory";
 
 const FormSection03 = ({ formChecking, setFormsChecking }) => {
   const [formValues, handleInputChange, resetForm] =
@@ -41,11 +42,6 @@ const FormSection03 = ({ formChecking, setFormsChecking }) => {
 
   const handleAddNewTag = (e) => {};
 
-  const handleInputValidation = (e) => {
-    handleInputChange(e);
-    section03Validator(e, setErrorsState);
-  };
-
   const handleFormSection_03Submit = (e) => {
     e.preventDefault();
     // const errorReport = Section01Validator(formValues);
@@ -56,67 +52,18 @@ const FormSection03 = ({ formChecking, setFormsChecking }) => {
     // }
   };
 
-  const handleSelectImageToLoad = () => {};
-
   const handleResetForm = (e) => {
     e.preventDefault();
     resetForm(section_03FormValues);
     setErrorsState(section_03ErrorState);
   };
 
-  const handleSubmitNewCategory = (e) => {
-    e.preventDefault();
-  };
-
   return (
     <div>
-      <h2 className="store-setup__section-enum mt-5">
-        3. Creación de los productos para la venta
-      </h2>
-      <form onsubmit={handleSubmitNewCategory}>
-        <div className="store-setup__product-category-container">
-          <label htmlFor="productCategory" className="store-setup__input-label">
-            Nombre de la categoria
-          </label>
-          <input
-            type="text"
-            name="productCategory"
-            id="productCategoryName"
-            className="store-setup__input"
-            autoComplete="off"
-            value={categoryName}
-            onChange={handleInputValidation}
-          />
-          <div className="store-input-image-container mt-3">
-            <label
-              htmlFor="productCategory"
-              className="store-setup__input-label"
-            >
-              Imágen representativa
-            </label>
-            <button
-              className="store-setup__category-img-button"
-              id="category-button"
-              onClick={handleSelectImageToLoad}
-            >
-              Carga un archivo
-            </button>
-            <input
-              type="file"
-              name="categoryImage"
-              className="store-setup__input-images"
-              id="store-setup__input-physic-img"
-              value={categoryImage}
-              onChange={handleInputValidation}
-            />
-          </div>
-          <div className="store-setup__centered-container">
-            <button className="store-setup__button-update" type="submit">
-              Agregar nueva categoría
-            </button>
-          </div>
-        </div>
-      </form>
+      <InputProductCategory
+        handleInputChange={handleInputChange}
+        setErrorsState={setErrorsState}
+      />
     </div>
   );
 };
