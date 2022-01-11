4ff53458300284f75efb042981fb8db932932aee
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-01-11 00:54:18 -0500

    Refactoring estilos en form 3 store setup

--- src/pages/storeSetup/FormSection03.jsx
+++ src/pages/storeSetup/FormSection03.jsx
@@ -90,251 +90,270 @@ const FormSection03 = ({ formChecking, setFormsChecking }) => {
         errorsState={errorsState}
         setErrorsState={setErrorsState}
       />
-      <form onSubmit={handleFormSection_03Submit}>
-        <div className="store-setup__form-container">
-          <div className="store-setup__inputs-container">
-            <div className="store-setup__input-container">
-              <label htmlFor="productName" className="store-setup__input-label">
-                Nombre del producto
-              </label>
-              <input
-                type="text"
-                autoFocus="true"
-                name="productName"
-                id="productName"
-                className="store-setup__input"
-                autoComplete="off"
-                value={productName}
-                onChange={handleInputValidation}
-              />
-            </div>
-            <div className="store-setup__error-flag mt-2 mb-4">
-              {errorsState.productName.hasErrors && (
-                <ErrorFlag
-                  message={errorsState.productName.message}
-                  width="100%"
-                />
-              )}
-            </div>
 
-            <div className="store-setup__input-container">
-              <label htmlFor="quantity" className="store-setup__input-label">
-                Cantidad disponible
-              </label>
-              <input
-                type="number"
-                name="quantity"
-                id="quantity"
-                value={quantity}
-                onChange={handleInputValidation}
-                className="store-setup__input"
-                autoComplete="off"
-                min="1"
-              />
-            </div>
-            <div className="store-setup__error-flag">
-              {errorsState.quantity.hasErrors && (
-                <ErrorFlag
-                  message={errorsState.quantity.message}
-                  width="100%"
+      <div style={{ margin: "10px 1%" }}>
+        <hr />
+      </div>
+      {categoriesList.length > 0 ? (
+        <form
+          onSubmit={handleFormSection_03Submit}
+          className="store-setup__form-main-container my-3"
+        >
+          <div className="store-setup__form-container">
+            <div className="store-setup__inputs-container">
+              <div className="store-setup__input-container">
+                <label
+                  htmlFor="productName"
+                  className="store-setup__input-label"
+                >
+                  Nombre del producto
+                </label>
+                <input
+                  type="text"
+                  autoFocus="true"
+                  name="productName"
+                  id="productName"
+                  className="store-setup__input"
+                  autoComplete="off"
+                  value={productName}
+                  onChange={handleInputValidation}
                 />
-              )}
-            </div>
+              </div>
+              <div className="store-setup__error-flag mt-2 mb-4">
+                {errorsState.productName.hasErrors && (
+                  <ErrorFlag
+                    message={errorsState.productName.message}
+                    width="100%"
+                  />
+                )}
+              </div>
 
-            <div className="store-setup__input-container">
-              <label
-                htmlFor="productState"
-                className="store-setup__input-label"
-              >
-                Estado
-              </label>
-              <select
-                name="productState"
-                id="productState"
-                value={productState}
-                onChange={handleInputValidation}
-                className="store-setup__input"
-                autoComplete="off"
-              >
-                {productStates.map((state) => (
-                  <option value={state.statusNumber} key={state.statusNumber}>
-                    {`(${state.statusNumber}) `}
-                    {state.statusName}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="store-setup__error-flag">
-              {errorsState.quantity.hasErrors && (
-                <ErrorFlag
-                  message={errorsState.quantity.message}
-                  width="100%"
+              <div className="store-setup__input-container">
+                <label htmlFor="quantity" className="store-setup__input-label">
+                  Cantidad disponible
+                </label>
+                <input
+                  type="number"
+                  name="quantity"
+                  id="quantity"
+                  value={quantity}
+                  onChange={handleInputValidation}
+                  className="store-setup__input"
+                  autoComplete="off"
+                  min="1"
                 />
-              )}
+              </div>
+              <div className="store-setup__error-flag">
+                {errorsState.quantity.hasErrors && (
+                  <ErrorFlag
+                    message={errorsState.quantity.message}
+                    width="100%"
+                  />
+                )}
+              </div>
+
+              <div className="store-setup__input-container">
+                <label
+                  htmlFor="productState"
+                  className="store-setup__input-label"
+                >
+                  Estado
+                </label>
+                <select
+                  name="productState"
+                  id="productState"
+                  value={productState}
+                  onChange={handleInputValidation}
+                  className="store-setup__input"
+                  autoComplete="off"
+                >
+                  {productStates.map((state) => (
+                    <option value={state.statusNumber} key={state.statusNumber}>
+                      {`(${state.statusNumber}) `}
+                      {state.statusName}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="store-setup__error-flag">
+                {errorsState.productState.hasErrors && (
+                  <ErrorFlag
+                    message={errorsState.productState.message}
+                    width="100%"
+                  />
+                )}
+              </div>
+            </div>
+            <div className="store-setup__inputs-container">
+              <div className="store-setup__input-container">
+                <label htmlFor="category" className="store-setup__input-label">
+                  Categoría
+                </label>
+                <select
+                  name="category"
+                  id="category"
+                  value={category}
+                  onChange={handleInputValidation}
+                  className="store-setup__input"
+                >
+                  {categoriesList.map((category) => (
+                    <option
+                      value={category.categoryName}
+                      className="store-setup__category-option"
+                    >
+                      {category.categoryName}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="store-setup__error-flag">
+                {errorsState.category.hasErrors && (
+                  <ErrorFlag
+                    message={errorsState.category.message}
+                    width="100%"
+                  />
+                )}
+              </div>
+
+              <div className="store-setup__input-container">
+                <label htmlFor="price" className="store-setup__input-label">
+                  Precio unitario
+                </label>
+                <input
+                  type="number"
+                  name="price"
+                  id="price"
+                  value={price}
+                  onChange={handleInputValidation}
+                  className="store-setup__input"
+                  autoComplete="off"
+                  min="1"
+                />
+                <select
+                  name="currency"
+                  id="currency"
+                  className="store-setup__input"
+                  value={currency}
+                  onChange={handleInputValidation}
+                >
+                  {latamCountries.map((country) => (
+                    <option value={country.currencyCode}>
+                      {country.currencyCode
+                        ? "[" +
+                          `${country.currencyCode}` +
+                          "]" +
+                          ` \t ${country.currencyName}`
+                        : "Seleccione la moneda"}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="store-setup__error-flag">
+                {errorsState.quantity.hasErrors && (
+                  <ErrorFlag
+                    message={errorsState.quantity.message}
+                    width="100%"
+                  />
+                )}
+              </div>
+
+              <div className="store-setup__input-container">
+                <label
+                  htmlFor="startingDate"
+                  className="store-setup__input-label"
+                >
+                  Fecha de apertura
+                </label>
+                <input
+                  type="date"
+                  name="startingDate"
+                  id="startingDate"
+                  onChange={handleInputValidation}
+                  className="store-setup__input store-setup__input-date"
+                  autoComplete="off"
+                />
+                <label
+                  htmlFor="endingDate"
+                  className="store-setup__input-date-label"
+                >
+                  Hasta
+                </label>
+                <input
+                  type="date"
+                  name="endingDate"
+                  id="endingDate"
+                  readOnly
+                  className="store-setup__input store-setup__input-date"
+                  autoComplete="off"
+                />
+              </div>
+              <div className="store-setup__error-flag">
+                {errorsState.quantity.hasErrors && (
+                  <ErrorFlag
+                    message={errorsState.quantity.message}
+                    width="100%"
+                  />
+                )}
+              </div>
+            </div>
+            <div className="store-setup__inputs-container">
+              <div className="store-setup__input-container">
+                <label className="store-setup__input-label">
+                  Dirección física
+                </label>
+                <button className="store-setup__input btn btn-primary store-setup__input-address">
+                  Obtener tu dirección actual
+                </button>
+              </div>
+              <div className="store-setup__error-flag">
+                {errorsState.quantity.hasErrors && (
+                  <ErrorFlag
+                    message={errorsState.quantity.message}
+                    width="100%"
+                  />
+                )}
+              </div>
+
+              <div className="store-setup__input-container">
+                <label className="store-setup__input-label">
+                  Dirección física
+                </label>
+                <button className="store-setup__input btn btn-primary store-setup__input-address">
+                  Obtener tu dirección actual
+                </button>
+              </div>
+              <div className="store-setup__error-flag">
+                {errorsState.quantity.hasErrors && (
+                  <ErrorFlag
+                    message={errorsState.quantity.message}
+                    width="100%"
+                  />
+                )}
+              </div>
             </div>
           </div>
-          <div className="store-setup__inputs-container">
-            <div className="store-setup__input-container">
-              <label htmlFor="category" className="store-setup__input-label">
-                Categoría
-              </label>
-              <select
-                name="category"
-                id="category"
-                value={category}
-                onChange={handleInputValidation}
-                className="store-setup__input"
+          <div className="store-setup__centered-container">
+            <div className="store-setup__buttons-container">
+              <button className="store-setup__button-update" type="submit">
+                Confirmar cambios
+              </button>
+              <button
+                className="store-setup__button-update"
+                onClick={handleResetForm}
               >
-                {categoriesList.map((category) => (
-                  <option
-                    value={category.categoryName}
-                    className="store-setup__category-option"
-                  >
-                    {category.categoryName}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="store-setup__error-flag">
-              {errorsState.category.hasErrors && (
-                <ErrorFlag
-                  message={errorsState.category.message}
-                  width="100%"
-                />
-              )}
-            </div>
-
-            <div className="store-setup__input-container">
-              <label htmlFor="price" className="store-setup__input-label">
-                Precio unitario
-              </label>
-              <input
-                type="number"
-                name="price"
-                id="price"
-                value={price}
-                onChange={handleInputValidation}
-                className="store-setup__input"
-                autoComplete="off"
-                min="1"
-              />
-              <select
-                name="currency"
-                id="currency"
-                className="store-setup__input"
-                value={currency}
-                onChange={handleInputValidation}
-              >
-                {latamCountries.map((country) => (
-                  <option value={country.currencyCode}>
-                    {country.currencyCode
-                      ? "[" +
-                        `${country.currencyCode}` +
-                        "]" +
-                        ` \t ${country.currencyName}`
-                      : "Seleccione la moneda"}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="store-setup__error-flag">
-              {errorsState.quantity.hasErrors && (
-                <ErrorFlag
-                  message={errorsState.quantity.message}
-                  width="100%"
-                />
-              )}
-            </div>
-
-            <div className="store-setup__input-container">
-              <label
-                htmlFor="startingDate"
-                className="store-setup__input-label"
-              >
-                Fecha de apertura
-              </label>
-              <input
-                type="date"
-                name="startingDate"
-                id="startingDate"
-                onChange={handleInputValidation}
-                className="store-setup__input store-setup__input-date"
-                autoComplete="off"
-              />
-              <label
-                htmlFor="endingDate"
-                className="store-setup__input-date-label"
-              >
-                Hasta
-              </label>
-              <input
-                type="date"
-                name="endingDate"
-                id="endingDate"
-                readOnly
-                className="store-setup__input store-setup__input-date"
-                autoComplete="off"
-              />
-            </div>
-            <div className="store-setup__error-flag">
-              {errorsState.quantity.hasErrors && (
-                <ErrorFlag
-                  message={errorsState.quantity.message}
-                  width="100%"
-                />
-              )}
-            </div>
-          </div>
-          <div className="store-setup__inputs-container">
-            <div className="store-setup__input-container">
-              <label className="store-setup__input-label">
-                Dirección física
-              </label>
-              <button className="store-setup__input btn btn-primary store-setup__input-address">
-                Obtener tu dirección actual
+                Resetear los datos
               </button>
             </div>
-            <div className="store-setup__error-flag">
-              {errorsState.quantity.hasErrors && (
-                <ErrorFlag
-                  message={errorsState.quantity.message}
-                  width="100%"
-                />
-              )}
-            </div>
-
-            <div className="store-setup__input-container">
-              <label className="store-setup__input-label">
-                Dirección física
-              </label>
-              <button className="store-setup__input btn btn-primary store-setup__input-address">
-                Obtener tu dirección actual
-              </button>
-            </div>
-            <div className="store-setup__error-flag">
-              {errorsState.quantity.hasErrors && (
-                <ErrorFlag
-                  message={errorsState.quantity.message}
-                  width="100%"
-                />
-              )}
-            </div>
           </div>
+        </form>
+      ) : (
+        <div style={{ margin: "10px 1%" }}>
+          <ErrorFlag
+            message="Ingrese al menos una categoría de producto para continuar"
+            width="100%"
+          />
         </div>
-        <div className="store-setup__centered-container">
-          <div className="store-setup__buttons-container">
-            <button className="store-setup__button-update" type="submit">
-              Confirmar cambios
-            </button>
-            <button
-              className="store-setup__button-update"
-              onClick={handleResetForm}
-            >
-              Resetear los datos
-            </button>
-          </div>
-        </div>
-      </form>
+      )}
     </div>
   );
 };
