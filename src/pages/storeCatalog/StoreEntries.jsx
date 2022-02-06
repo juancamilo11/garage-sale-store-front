641f983082d28ef4b08f6da886cd9e6df02d4eaa
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-02-05 23:35:21 -0500

    Added styles for store catalog

--- src/pages/storeCatalog/StoreEntries.jsx
+++ src/pages/storeCatalog/StoreEntries.jsx
@@ -6,11 +6,12 @@ const StoreEntries = () => {
   const { stores } = useSelector((state) => state);
   return (
     <div className="journal__entries">
-      {stores
+      {stores.garageSaleStores
         .sort((store1, store2) => store2.date - store1.date)
         .map((note) => (
           <StoreEntry key={note.id} {...note} />
         ))}
+      {JSON.stringify(stores)}
     </div>
   );
 };
