5da4a3f3cf7c19a3c32be6d15a0de0a915483d61
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-02-07 03:13:46 -0500

    Added styles for storeCatalog navbar

--- src/pages/storeCatalog/Sidebar.jsx
+++ src/pages/storeCatalog/Sidebar.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 import StoreEntries from "./StoreEntries";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const handleGoToProfile = () => {
+    navigate("/user-profile");
+  };
+
   const dispatch = useDispatch();
   const { auth } = useSelector((state) => state);
   return (
     <aside className="store-catalog__sidebar">
       <div className="store-catalog__sidebar-navbar">
-        <div className="store-catalog__sidebar-user-info">
+        <div
+          className="store-catalog__sidebar-user-info"
+          onClick={handleGoToProfile}
+        >
           {auth?.photoUrl ? (
             <img
               src={auth.photoUrl}
@@ -21,7 +31,11 @@ const Sidebar = () => {
           )}
           <span className="store-catalog__display-name"> {auth.name}</span>
         </div>
+        <button className="store-catalog__search-button">
+          Buscar y filtrar
+        </button>
       </div>
+
       <StoreEntries />
     </aside>
   );
