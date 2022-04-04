690d8fb53665cb6bcc3f24009e4ad6fed6ec4b64
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-04-04 00:50:42 -0500

    Env vars setup

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -1,21 +1,13 @@
 //------ Dev Variables -------
-const environment = {
-  msAdminHistoryUrl: "http://localhost:8080/api/v1",
-  msAdminInfoUserUrl: "http://localhost:8081/api/v1",
-  msAdminInvoicingUrl: "http://localhost:8082/api/v1",
-  msAdminSalesUrl: "http://localhost:8083/api/v1",
-  msAdminStoresUrl: "http://localhost:8084/api/v1",
-  msAdminTraceabilityUrl: "http://localhost:8085/api/v1",
-};
-
-//------ Prod Variables -------
-// export const environment = {
-//   msAdminHistoryUrl: "",
-//   msAdminInfoUserUrl: "",
-//   msAdminInvoicingUrl: "",
-//   msAdminSalesUrl: "",
-//   msAdminStoresUrl: "",
-//   msAdminTraceabilityUrl: "",
+// const environment = {
+//   msAdminInfoUserUrl: "http://localhost:8081/api/v1",
+//   msAdminStoresUrl: "http://localhost:8084/api/v1",
 // };
+
+// ------ Prod Variables -------
+export const environment = {
+  msAdminInfoUserUrl: "https://gss-admin-info-users.herokuapp.com/api/v1",
+  msAdminStoresUrl: "https://gss-admin-stores.herokuapp.com/api/v1",
+};
 
 export default environment;
